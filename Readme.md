750b053a6f1040cb26a99e369116fd2b21e7dc78
niki <hootan09@gmail.com>
2021-09-03 17:40:42 +0430

    home is done

---
+++ Readme.md
@@ -0,0 +1,3 @@
+### Travel App UI (android)
+
+![Home Screen](./assets/images/Home.jpg)
--- components/Home.js
+++ components/Home.js
@@ -10,27 +10,51 @@ import discoverCategoriesData from '../assets/data/discoverCategoriesData';
 import discoverData from '../assets/data/discoverData';
 import learnMoreData from '../assets/data/learnMoreData';
 
-import profileImg from '../assets/images/person.png'
+import profileImg from '../assets/images/person.png';
+import menuImg from '../assets/images/menu.png';
 
 Feather.loadFont();
 Entypo.loadFont();
 
 const Home = ({navigation}) => {
-    const renderDiscoverItem = (item) =>{
+    const renderDiscoverItem = ({item}) =>{
         return(
-            <TouchableOpacity>
+            <TouchableOpacity
+                onPress={()=>navigation.navigate("Details", {item: item})}
+            >
                 <ImageBackground 
-                    source={item.item.image}
+                    source={item.image}
                     style={styles.discoverItem}
                     imageStyle={styles.discoverItemImage}
                 >
-                    <Text style={styles.discoverItemTitle}>{item.item.title}</Text>
+                    <Text style={styles.discoverItemTitle}>{item.title}</Text>
                     <View style={styles.discoverItemLocationWrapper}>
                         <Entypo name="location-pin" size={18} color={colors.white}/>
-                        <Text style={styles.discoverItemLocationText}>{item.item.location}</Text>
+                        <Text style={styles.discoverItemLocationText}>{item.location}</Text>
                     </View>
                 </ImageBackground>
             </TouchableOpacity>
+        )
+    };
+
+    const renderActivityItem = ({item}) =>{
+        return(
+            <View style={styles.activityItemWrapper}>
+                <Image source={item.image} style={styles.activityItemImage}/>
+                <Text style={styles.activityItemText}>{item.title}</Text>
+            </View>
+        )
+    };
+
+    const renderlearnMoreItem = ({item}) => {
+        return(
+            <ImageBackground 
+                source={item.image} 
+                style={styles.learnMoreItem}
+                imageStyle={styles.learnMoreItemImage}
+            >
+                <Text style={styles.learmMoreitemText}>{item.title}</Text>
+            </ImageBackground>
         )
     }
     return (
@@ -39,7 +63,8 @@ const Home = ({navigation}) => {
             <ScrollView>
                 {/* Header */}
                 <View style={styles.menuWrapper}>
-                    <Feather name="menu" size={32} color={colors.black} style={styles.menuIcon} />
+                    {/* <Feather name="menu" size={32} color={colors.black} style={styles.menuIcon} /> */}
+                    <Image source={menuImg} style={styles.menuIcon}/>
                     <Image source={profileImg} style={styles.profileImage}/>
                 </View>
                 
@@ -63,7 +88,33 @@ const Home = ({navigation}) => {
                     />
                 </View>
 
+                {/* Activities */}
+                <View style={styles.activitiesWrapper}>
+                    <Text style={styles.activitiesTitle}>Activities</Text>
+                    <View style={[styles.activitiesItemsWrapper, {marginLeft: 20,}]}>
+                        <FlatList 
+                            data={activitiesData}
+                            renderItem={renderActivityItem}
+                            keyExtractor={(item)=> item.id}
+                            horizontal
+                            showsHorizontalScrollIndicator={false}
+                        />
+                    </View>
+                </View>
 
+                {/* LearMore */}
+                <View style={styles.learnMoreWrapper}>
+                    <Text style={styles.learnMoreTitle}>Learn More</Text>
+                    <View style={[styles.learnMoreItemsWrapper, {marginLeft: 20,}]}>
+                        <FlatList 
+                            data={learnMoreData}
+                            renderItem={renderlearnMoreItem}
+                            keyExtractor={(item)=> item.id}
+                            horizontal
+                            showsHorizontalScrollIndicator={false}
+                        />
+                    </View>
+                </View>
             </ScrollView>
         </SafeAreaView>
     )
@@ -87,7 +138,10 @@ const styles = StyleSheet.create({
         height: 52,
         borderRadius: 10,
     },
-    menuIcon: {},
+    menuIcon: {
+        width:24,
+        height: 24,
+    },
     discoverWrapper: {
         marginHorizontal: 20,
     },
@@ -108,10 +162,10 @@ const styles = StyleSheet.create({
     discoverItemsWrapper: {
         marginHorizontal: 20,
         paddingVertical: 20,
-        marginTop: 20,
+        // marginTop: 10,
     },
     discoverItem: {
-        width: 170,
+        width: 160,
         height: 250,
         justifyContent: "flex-end",
         paddingHorizontal: 10,
@@ -137,6 +191,70 @@ const styles = StyleSheet.create({
         color: colors.white,
         fontFamily: "Lato-Bold",
         fontSize: 14,
+    },
+    activitiesWrapper: {
+        marginTop: 10,
+    },
+    activitiesTitle: {
+        marginHorizontal: 20,
+        fontFamily: "Lato-Bold",
+        fontSize: 24,
+        color: colors.black,
+
+    },
+    activitiesItemsWrapper: {
+        paddingVertical: 20,
+    },
+    activityItemWrapper: {
+        justifyContent: "flex-end",
+        alignItems: "center",
+        marginRight: 25,
+
+    },
+    activityItemImage: {
+        width: 36,
+
+    },
+    activityItemText: {
+        marginTop: 5,
+        fontFamily: "Lato-Bold",
+        fontSize: 14,
+        color: colors.gray,
+    },
+    learnMoreWrapper: {
+        marginTop: 20,
+    },
+    learnMoreTitle: {
+        marginHorizontal: 20,
+        fontFamily: "Lato-Bold",
+        fontSize: 24,
+        color:colors.black
+    },
+    learnMoreWrapper: {},
+    learnMoreTitle: {
+        marginHorizontal: 20,
+        fontFamily: "Lato-Bold",
+        fontSize: 24,
+        color: colors.black,
+    },
+    learnMoreItemsWrapper: {
+        marginTop: 20,
+    },
+    learnMoreItem: {
+        width: 170,
+        height: 180,
+        justifyContent: "flex-end",
+        marginRight: 20,
+    },
+    learnMoreItemImage: {
+        borderRadius: 20,
+    },
+    learmMoreitemText: {
+        fontFamily: "Lato-Bold",
+        fontSize: 18,
+        color: colors.white,
+        marginHorizontal: 20,
+        marginVertical: 20,
     },
 })
 
